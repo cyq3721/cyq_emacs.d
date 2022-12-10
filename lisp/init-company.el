110dff59a762a1235b71769ca1fb61f9cd68fc2d
cyq3721 <cyq3721@hotmail.com>
2022-12-10 21:41:10 +0800

    emacs

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,3 +1,6 @@
+;;; package --- Summary
+;;; Commentary:
+;;; code:
 (require 'company)
 ;;(require 'flycheck )
 ;;(global-flycheck-mode t)
@@ -11,21 +14,22 @@
 
 (yas-global-mode 1)
 (add-hook 'after-init-hook 'global-company-mode)          ;;开启补全
- (setq company-dabbrev-code-everywhere t 
-        company-dabbrev-code-modes t 
-        company-dabbrev-code-other-buffers 'all 
-        company-dabbrev-downcase nil 
-        company-dabbrev-ignore-case t 
-        company-dabbrev-other-buffers 'all 
-        company-require-match nil 
+(setq company-dabbrev-code-everywhere t
+        company-dabbrev-code-modes t
+        company-dabbrev-code-other-buffers 'all
+        company-dabbrev-downcase nil
+        company-dabbrev-ignore-case t
+        company-dabbrev-other-buffers 'all
+        company-require-match nil
         company-minimum-prefix-length 1
-        company-show-numbers t 
-        company-tooltip-limit 20 
-        company-idle-delay 0 
-        company-echo-delay 0 
-        company-tooltip-offset-display 'scrollbar 
-        company-begin-commands '(self-insert-command)) 
+        company-show-numbers t
+        company-tooltip-limit 20
+        company-idle-delay 0
+        company-echo-delay 0
+        company-tooltip-offset-display 'scrollbar
+        company-begin-commands '(self-insert-command))
 (push '(company-semantic :with company-yasnippet) company-backends)
 
 
 (provide 'init-company)
+;;; init-company.el ends here
